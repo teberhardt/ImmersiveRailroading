c249d8716d46048d06e7461c44a997b7fddc4697
Christian Mesh <christianmesh1@gmail.com>
2021-08-30 18:20:18 -0400

    Fix control scaling (scale around group center)

--- src/main/java/cam72cam/immersiverailroading/model/part/Control.java
+++ src/main/java/cam72cam/immersiverailroading/model/part/Control.java
@@ -90,7 +90,7 @@ public class Control {
     }
 
     public void render(EntityRollingStock stock, ComponentRenderer draw) {
-        if (rotationPoint == null && translations.isEmpty()) {
+        if (rotationPoint == null && translations.isEmpty() && scales.isEmpty()) {
             draw.render(part);
             return;
         }
@@ -116,11 +116,13 @@ public class Control {
                 GL11.glTranslated(-rotationPoint.x, -rotationPoint.y, -rotationPoint.z);
             }
             if (!scales.isEmpty()) {
+                GL11.glTranslated(part.center.x, part.center.y, part.center.z);
                 GL11.glScalef(
                         scales.containsKey(Axis.X) ? scales.get(Axis.X) * valuePercent : 1,
                         scales.containsKey(Axis.Y) ? scales.get(Axis.Y) * valuePercent : 1,
                         scales.containsKey(Axis.Z) ? scales.get(Axis.Z) * valuePercent : 1
                 );
+                GL11.glTranslated(-part.center.x, -part.center.y, -part.center.z);
             }
             matrix.render(part);
         }
@@ -183,6 +185,15 @@ public class Control {
                     rotations.getOrDefault(Axis.Z, 0f)
             );
             m = m.translate(-rotationPoint.x, -rotationPoint.y, -rotationPoint.z);
+        }
+        if (!scales.isEmpty()) {
+            m = m.translate(part.center.x, part.center.y, part.center.z);
+            m = m.scale(
+                    scales.containsKey(Axis.X) ? scales.get(Axis.X) * valuePercent : 1,
+                    scales.containsKey(Axis.Y) ? scales.get(Axis.Y) * valuePercent : 1,
+                    scales.containsKey(Axis.Z) ? scales.get(Axis.Z) * valuePercent : 1
+            );
+            m = m.translate(-part.center.x, -part.center.y, -part.center.z);
         }
         return m.apply(point);
     }
